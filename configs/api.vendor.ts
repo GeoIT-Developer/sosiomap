46e36131397815f876911b090d6878e7f49a1b6e
Alf-Anas <alfadila.anas@gmail.com>
2024-02-14 04:56:38 +0700

    remove import

--- configs/api.vendor.ts
+++ configs/api.vendor.ts
@@ -1,12 +1,4 @@
-import { getCookie } from 'cookies-next';
 import axios from 'axios';
-import { COOKIE, LOCAL_STORAGE } from '@/utils/constant';
-import { encrypt } from '@/utils/helper';
-import { GetPublicChatParamsInterface } from '@/types/api/params/get-public-chat.interface';
-import { PostChatParamsInterface } from '@/types/api/params/post-chat.interface';
-import { PostPostParamsInterface } from '@/types/api/params/post-post.interface';
-import { GetPublicMapPostParamsInterface } from '@/types/api/params/get-public-map-post.interface';
-import { PostCommentParamsInterface } from '@/types/api/params/post-comment.interface';
 
 const HOST = axios.create();
 
