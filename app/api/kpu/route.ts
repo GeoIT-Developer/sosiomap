ece4abee289877334f8c3b2ee5739b4eca70cb68
Alf-Anas <alfadila.anas@gmail.com>
2024-02-15 03:27:31 +0700

    Test API

--- app/api/kpu/route.ts
+++ app/api/kpu/route.ts
@@ -2,9 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
     try {
+        console.log('GETTTT OK');
         const id = req.nextUrl.searchParams.get('id') as string;
         const timestamp = req.nextUrl.searchParams.get('timestamp') as string;
         const type = req.nextUrl.searchParams.get('type') as string;
+        console.log('PARAMS');
+        console.log(id);
+        console.log(timestamp);
+        console.log(type);
         let kpuURL = '';
         if (type === 'DPD') {
             kpuURL = `https://infopemilu.kpu.go.id/Pemilu/Dct_dpd/dct_dpd?kode_pro=${id}&_=${timestamp}`;
@@ -15,7 +20,11 @@ export async function GET(req: NextRequest) {
         } else if (type === 'DPRD_KAB_KOTA') {
             kpuURL = `https://infopemilu.kpu.go.id/Pemilu/Dct_dprd/Dct_dprdkabko?kode_dapil=${id}&_=${timestamp}`;
         }
+        console.log('kpuURL');
+        console.log(kpuURL);
         const response = await fetch(kpuURL);
+        console.log('response');
+        console.log(response);
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
