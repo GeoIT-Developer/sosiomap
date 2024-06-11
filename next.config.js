01872efe327195f0102217fa759ff08f571502cf
Alf-Anas <alfadila.anas@gmail.com>
2024-06-11 15:33:26 +0700

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-// Disable PWA for Development
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-eval' 'unsafe-inline';
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' blob: data:;
+//     font-src 'self';
+//     object-src 'none';
+//     base-uri 'self';
+//     form-action 'self';
+//     frame-ancestors 'none';
+//     upgrade-insecure-requests;
+// `;
+
 const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
@@ -14,9 +26,42 @@ const nextConfig = {
         NEXTAUTH_REALM_IAM: process.env.NEXTAUTH_REALM_IAM,
         NEXTAUTH_REDIRECT_URI: process.env.NEXTAUTH_REDIRECT_URI,
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains',
+                    },
+                    // {
+                    //     key: 'Content-Security-Policy',
+                    //     value: cspHeader.replace(/\n/g, ''),
+                    // },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer-when-downgrade',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(self), microphone=(self), camera=(self), fullscreen=(self), payment=(self), vr=(), xr-spatial-tracking=(self)',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
-// module.exports = nextConfig;
+// Disable PWA for Development
 module.exports =
     process.env.NEXT_PUBLIC_STAGE === 'PRODUCTION'
         ? withPWA(nextConfig)
